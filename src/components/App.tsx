b523d63932665f45aba72fa4f1f6cad1b2335aee
aclaudel <arnaud.claudel@codurance.com>
2020-10-07 22:00:42 +0200

    first failing acceptance test

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class App extends React.Component<any, any> {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    throw new Error('App component not yet implemented')
+    return <div />
+  }
+}
