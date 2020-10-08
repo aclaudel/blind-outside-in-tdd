9535b922cfc79bc40a1ac7a568f7ecbbc83026d1
aclaudel <arnaud.claudel@codurance.com>
2020-10-08 11:04:27 +0200

    make test pass

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import MainSection from "./sections/MainSection";
 
 export default class App extends React.Component<any, any> {
   constructor(props) {
     super(props);
+    this.state = {
+      isLoading: false,
+      items: []
+    }
   }
 
   render() {
-    return <div />
+    return <MainSection isLoading={this.state.isLoading}
+                        items={this.state.items}/>;
   }
 }
--- src/components/sections/MainSection.tsx
+++ src/components/sections/MainSection.tsx
@@ -2,4 +2,5 @@ import React from "react";
 
 export default function MainSection(props) {
   throw new Error('Main section component not yet implemented')
+  return <div />
 }
