bfbafa12d46d3cc552710222fcd43d7540dbf6ae
aclaudel <arnaud.claudel@codurance.com>
2020-10-08 11:36:12 +0200

    no test for Loading section

--- src/components/sections/loading/LoadingSection.tsx
+++ src/components/sections/loading/LoadingSection.tsx
@@ -2,6 +2,5 @@ import React from "react";
 import {Spinner} from "react-bootstrap";
 
 export default function LoadingSection(props) {
-  throw new Error('Loading Section component not yet implemented')
   return <div />
 }
