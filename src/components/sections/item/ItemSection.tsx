774a66710aa0d57a5304ac8a030e4253809d05cf
aclaudel <arnaud.claudel@codurance.com>
2020-10-08 11:46:42 +0200

    should render items

--- src/components/sections/item/ItemSection.tsx
+++ src/components/sections/item/ItemSection.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import {Alert, Card} from "react-bootstrap";
 
 export default function ItemSection(props) {
-  throw new Error('Item Section component not yet implemented')
-  return <div />
+  return (
+    <div>
+      {props.items.map(item =>
+        <div data-testid="item" key={item.id} />)}
+    </div>
+  );
 }
